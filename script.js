258ccf98613acbce11fcf4e50cfa530c07206732
SidneyPF <sidpedroso2017@gmail.com>
2024-11-09 13:00:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showIncon(image){
+    const icon = image.nextElementSibling;
+    icon.style.display = 'block';
+}
+
+function hideIcon(image){
+    const icon = image.nextElementSibling;
+    icon.style.display = 'none';
+}
+
+function setaa(){
+    document.getElementById('setaLuta').style.display = 'block';
+}
+
+function setae(){
+    document.getElementById('setaLuta').style.display = 'none';
+}
