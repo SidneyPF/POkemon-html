4f6e60a9cff422bf37927c7490873bde860ee0de
SidneyPF <sidpedroso2017@gmail.com>
2024-11-16 13:02:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let vida1 = 166;
+let vida2 = 200;
+vidaCompletaOponente = (vida1*100)/90
+
+
 function showIncon(image){
     const icon = image.nextElementSibling;
     icon.style.display = 'block';
@@ -14,4 +19,56 @@ function setaa(){
 
 function setae(){
     document.getElementById('setaLuta').style.display = 'none';
+}
+
+function ataque(){
+    document.getElementById('ataqueJogador').style.display = 'block';
+}
+
+function esconderAtaque(){
+    document.getElementById('ataqueJogador').style.display = 'none';
+}
+
+function ataque1(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    console.log(vida1)
+ 
+    document.getElementById('vidaPkmOponente').style.width= `${novaVidaP}%`;
+}
+
+function ataque2(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    console.log(vida1)
+ 
+    document.getElementById('vidaPkmOponente').style.width= `${novaVidaP}%`;
+}
+
+function ataque3(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    console.log(vida1)
+ 
+    document.getElementById('vidaPkmOponente').style.width= `${novaVidaP}%`;
+}
+
+function ataque4(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    console.log(vida1)
+ 
+    document.getElementById('vidaPkmOponente').style.width= `${novaVidaP}%`;
 }
